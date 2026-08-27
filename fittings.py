b9054bf9e65cf96b664b31300fd9e1afe73203d6
AhmedAl91 <89253021+AhmedAl91@users.noreply.github.com>
2026-08-27 17:51:07 +0100

    Create fittings.py

---
+++ fittings.py
@@ -0,0 +1,6 @@
+K_VALUES = {
+    # upper and low bounds for k values
+    "pipe_bend_45_degrees": [0.15, 0.40],
+    "pipe_bend_90_degrees_long": [0.20, 0.40],
+    "pipe_bend_90_degrees_short": [0.50, 1.00],
+}
