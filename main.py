dbe28a2ca7d8c272eae67b85c15cd9a61c3e6bf0
AhmedAl91 <89253021+AhmedAl91@users.noreply.github.com>
2026-08-27 09:10:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,22 @@ import math
 # Constants
 G = 9.81         # m2/s
 
-# Water parameters
-NU = 1.0e-6      # m2/s, approximate water kinematic viscosity
+# Water parameters @ 20 C
+NU = 1.0e-6      # m2/s, approximate kinematic viscosity
+RHO = 998        # kg/m3, approximate density
+
+# Declare nodes
+nodes = {}
+
+# Declare pipes and ducts
+pipes = {
+  # Give Id to pipe
+  "P01": {
+    # Fixed characteristics
+    "length": 10.0,               # m
+    "diameter": 0.75,             # m, internal diameter
+    "roughness": 0.0001,          # m = 0.1 mm, typical cast iron pipe
+    "resistance": 0.0,            # to be determined from sum of fittings
+  }
+}
+
