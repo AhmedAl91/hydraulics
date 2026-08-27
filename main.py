728438b328028e188b1843612bf0c393dd046c47
AhmedAl91 <89253021+AhmedAl91@users.noreply.github.com>
2026-08-27 13:48:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #  Libraries & packages
-import math
+import math      # for basic mathematic operations
 
 # Constants
 G = 9.81         # m2/s
@@ -16,10 +16,22 @@ pipes = {
   # Give Id to pipe
   "P01": {
     # Fixed characteristics
-    "length": 10.0,               # m
-    "diameter": 0.75,             # m, internal diameter
-    "roughness": 0.0001,          # m = 0.1 mm, typical cast iron pipe
-    "resistance": 0.0,            # to be determined from sum of fittings
+    "length": 10.0,                            # m
+    "diameter": 0.75,                          # m, internal diameter
+    "roughness": 0.0001,                       # m = 0.1 mm, typical cast iron pipe
+    "k_values_fittings": 0.0,                  # i.e. resistance coefficients, to be determined from sum of fittings
   }
 }
+
+
+# Utility methods
+def area(diameter):
+  return math.pi * diameter**2 / 4
+
+def velocity(flow, diameter):
+  return flow / area(diameter)
+
+def reynolds_number(flow, diameter):
+  v = velocity(flow, diameter)
+  return v * diameter / NU
 
