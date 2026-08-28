0c88b44cf1cdc3eefe175bb366776daa9ffb6586
AhmedAl91 <89253021+AhmedAl91@users.noreply.github.com>
2026-08-28 09:45:49 +0100

    k values

--- fittings.py
+++ fittings.py
@@ -1,4 +1,27 @@
 K_VALUES = {
+    
+    # Entry losses
+    "pipe_entry_sharp_edged": {
+        "lower": 0.50,
+        "upper": 0.50,
+    },
+    "pipe_entry_re_entrant": {
+        "lower": 0.80,
+        "upper": 0.80,
+    },
+    "pipe_entry_slightly_rounded": {
+        "lower": 0.25,
+        "upper": 0.25,
+    },
+    "pipe_entry_bellmouth": {
+        "lower": 0.05,
+        "upper": 0.05,
+    },
+    "pipe_entry_foot_valve_and_strainer": {
+        "lower": 2.5,
+        "upper": 2.5,
+    },
+    # Bends
     "pipe_bend_45_degrees": {
         "lower": 0.15,
         "upper": 0.40,
