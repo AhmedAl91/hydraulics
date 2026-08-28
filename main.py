f1937fd5dcdfaa46920c3d8b1b14c35cf3360590
AhmedAl91 <89253021+AhmedAl91@users.noreply.github.com>
2026-08-28 09:26:56 +0100

    init

--- main.py
+++ main.py
@@ -57,13 +57,7 @@ def plot_system_curves(pipes):
       label="Available head (m)"
   )
   plt.title("System Resistance Curves")
-  plt.fill_between(
-    flows,
-    lower_losses,
-    upper_losses,
-    alpha=0.2,
-    label="Resistance uncertainty"
-  )
+  
   plt.grid()
   plt.legend()
 
