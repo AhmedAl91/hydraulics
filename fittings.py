11e09d11a806c48120b93f563f1b802da2bf3c44
AhmedAl91 <89253021+AhmedAl91@users.noreply.github.com>
2026-08-28 10:20:57 +0100

    fittings

--- fittings.py
+++ fittings.py
@@ -21,19 +21,84 @@ K_VALUES = {
         "lower": 2.5,
         "upper": 2.5,
     },
-    # Bends
-    "pipe_bend_45_degrees": {
-        "lower": 0.15,
-        "upper": 0.40,
-    },
-
-    "pipe_bend_90_degrees_long": {
+    # Elbows
+    "pipe_elbow_22_5_degrees": {
         "lower": 0.20,
+        "upper": 0.20,
+    },
+    "pipe_elbow_45_degrees": {
+        "lower": 0.40,
         "upper": 0.40,
     },
-
-    "pipe_bend_90_degrees_short": {
-        "lower": 0.50,
+    "pipe_elbow_90_degrees": {
+        "lower": 1.00,
         "upper": 1.00,
+    },
+    # Bends - short : 1D, long: 2 to 7D, sweep: 8 to 50D
+    "pipe_bend_22_5_degrees_short": {
+        "lower": 0.15,
+        "upper": 0.15,
+    },
+    "pipe_bend_45_degrees_short": {
+        "lower": 0.30,
+        "upper": 0.30,
+    },
+    "pipe_bend_90_degrees_short": {
+        "lower": 0.75,
+        "upper": 0.75,
+    },
+    "pipe_bend_22_5_degrees_long": {
+        "lower": 0.10,
+        "upper": 0.10,
+    },
+    "pipe_bend_45_degrees_long": {
+        "lower": 0.20,
+        "upper": 0.20,
+    },
+    "pipe_bend_90_degrees_long": {
+        "lower": 0.40,
+        "upper": 0.40,
+    },
+    "pipe_bend_22_5_degrees_sweep": {
+        "lower": 0.05,
+        "upper": 0.05,
+    },
+    "pipe_bend_45_degrees_sweep": {
+        "lower": 0.10,
+        "upper": 0.10,
+    },
+    "pipe_bend_90_degrees_sweep": {
+        "lower": 0.20,
+        "upper": 0.20,
+    },
+    # Tees
+    "pipe_tee_in_line": {
+        "lower": 0.35,
+        "upper": 0.35,
+    },
+    "pipe_tee_branch_radiused": {
+        "lower": 0.8,
+        "upper": 0.8,
+    },
+    "pipe_tee_branch_sharp": {
+        "lower": 1.20,
+        "upper": 1.20,
+    },
+    # Angled branches
+    "pipe_angle_in_line": {
+        "lower": 0.35,
+        "upper": 0.35,
+    },
+    "pipe_angle_branch_30_degrees": {
+        "lower": 0.40,
+        "upper": 0.40,
+    },
+    "pipe_angle_branch_45_degrees": {
+        "lower": 0.60,
+        "upper": 0.60,
+    },
+    "pipe_tee_branch_90_degrees": {
+        "lower": 0.80,
+        "upper": 0.80,
     },
 }
