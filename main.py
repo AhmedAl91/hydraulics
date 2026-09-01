82b6dd200e129044f148ae7e74691a7d9c9abf46
Ahmed Al-Aaraji <ahmed.al-aaraji@aecom.com>
2026-09-01 17:02:26 +0100

    init

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import system                                # input data: pipes and ducts
 G = 9.81                                     # m2/s
 
 # Declare system design inputs
+nodes = system.nodes
 pipes = system.pipes
 ducts = system.ducts
-max_flow = system.max_flow                   # l/s
-available_head = system.available_head       # m
+flow_cases = system.flow_cases                # m3/s
 
 def pipe_headloss(flow, pipe, boundary):
   # Return friction, fitting and total headloss for one pipe
@@ -24,59 +24,60 @@ def pipe_headloss(flow, pipe, boundary):
   
   f = hydraulics.friction_factor(flow, diameter, roughness)
   
-  friction_loss = ( f * length / diameter * v**2 / (2 * G) )          # f (L/D) * (v^2/2g)
+  friction_loss = (f * length / diameter * v**2 / (2 * G))          # f (L/D) * (v^2/2g)
   fittings_loss = k * v**2 / (2 * G)                                  # K * (v^2/2g)   
   
   return friction_loss + fittings_loss
 
-def total_headloss(flow, pipes, boundary):
-  # Return total headloss through pipes arranged in series
-  total_loss = 0.0
-  
-  for pipe_id, pipe in pipes.items():
-    result = pipe_headloss(flow, pipe, boundary)
-    total_loss += result
-    
-  return total_loss    
+def total_headloss(flow, pipe, boundary):
+  # Return total headloss through pipe
+  return pipe_headloss(flow, pipe, boundary)    
 
-def plot_system_curves(pipes):
+def plot_system_curves(pipes, case="average"):
   # plot head losses against a range of flows  
+  for pipe_id, pipe in pipes.items():
+
+    available_head = nodes[pipe["from_node"]]["aod"] - nodes[pipe["to_node"]]["aod"]  # m
+
+    max_flow = flow_cases[case][pipe_id] + 1                      # m3/s
+
+    flows = [q for q in range(1, max_flow)]                       # m3/s
+    
+    lower_losses = [total_headloss(q, pipe, "lower") for q in flows]
+    
+    upper_losses = [total_headloss(q, pipe, "upper") for q in flows]
   
-  flows = [q / 1000 for q in range(1, max_flow)]                # assign m3/s
-
-  lower_losses = [total_headloss(q, pipes, "lower") for q in flows]
+    plt.plot(flows, lower_losses, label="Lower resistance")
+    
+    plt.plot(flows, upper_losses, label="Upper resistance")
   
-  upper_losses = [total_headloss(q, pipes, "upper") for q in flows]
-
-  plt.plot(flows, lower_losses, label="Lower resistance")
+    plt.xlabel("Flow (m³/s)")
+    plt.ylabel("Headloss (m)")
+    # Intersect system curve with available head
+    plt.axhline(
+        y=available_head,
+        label="Available head (m)"
+    )
+    plt.title("System Resistance Curves")
+    plt.fill_between(
+      flows,
+      lower_losses,
+      upper_losses,
+      alpha=0.2,
+      label="Resistance uncertainty"
+    )
+    plt.grid()
+    plt.legend()
   
-  plt.plot(flows, upper_losses, label="Upper resistance")
-
-  plt.xlabel("Flow (m³/s)")
-  plt.ylabel("Headloss (m)")
-  # Intersect system curve with available head
-  plt.axhline(
-      y=available_head,
-      label="Available head (m)"
-  )
-  plt.title("System Resistance Curves")
-  plt.fill_between(
-    flows,
-    lower_losses,
-    upper_losses,
-    alpha=0.2,
-    label="Resistance uncertainty"
-  )
-  plt.grid()
-  plt.legend()
-
-  plt.savefig(
-    "outputs/system_curve.png",
-    dpi=150,
-    bbox_inches="tight"
-  )
-
-  plt.show()
+    plt.savefig(
+      "outputs/system_curve.png",
+      dpi=150,
+      bbox_inches="tight"
+    )
+  
+    plt.show()
+  
+  
 
 # Call function
-plot_system_curves(pipes)
+plot_system_curves(pipes, "average")
--- system.py
+++ system.py
@@ -1,11 +1,50 @@
-# Declare pipes and ducts for a given flow (split if there are merging or diverging flows)
-max_flow = 501                                 # l/s
+# Declare nodes, junctions, pipes and ducts
 
-available_head = 0.05                          # m
+nodes = {
+    # Nxx for nodes, Jxx for tee / junctions  # aod: mm
+    "N01": {"aod": 100.0},                    
+    "N02": {"aod": 95.0},
+    "J01": {"aod": 85.0},
+    "N03": {"aod": 80.0},
+}
+
+# Example merging flows: Q3 = Q1 + Q2         # m3/s
+# Requires a manual check for continuity of flow at junctions, i.e. Q1 + Q2 = Q3
+flow_cases = {
+    "average": {
+      "P01": 0.050,
+      "P02": 0.030, 
+      "P03": 0.080, 
+    },   
+
+    "peak": {
+      "P01": 0.050,
+      "P02": 0.030, 
+      "P03": 0.080, 
+    },                                  
+}
+# Example diverging flows: Q1 = Q2 + Q3 
+# This needs rework as in reality the Q2 is an output from the head evaluated at the junction
+# q_in = 0.100
+
+# split = {
+#     "P02": 0.40,
+#     "P03": 0.60
+# }
+
+# flow_case_2 = {
+#     # can create multiple flow cases e.g. peak, average, min
+#     "P01": q_in,                              # m3/s
+#     "P02": q_in * split["P02"], 
+#     "P03": q_in * split["P03"],                         
+# }
 
 pipes = {
   "P01": {
     # Fixed characteristics
+    "from_node": "N01",
+    "to_node": "J01",
+
     "length": 10.0,                            # m
     "diameter": 0.75,                          # m, internal diameter
     "roughness": 0.0001,                       # m = 0.1 mm, typical cast iron pipe with concrete lining
@@ -15,13 +54,43 @@ pipes = {
       "pipe_bend_45_degrees", 
       "pipe_bend_90_degrees_long",
       "pipe_bend_90_degrees_long",
-      "pipe_bend_90_degrees_long",
-      "pipe_bend_90_degrees_short",
-      "pipe_bend_90_degrees_short",
-      "pipe_bend_90_degrees_short",
       "pipe_bend_90_degrees_short",
     ]
-  }
+  },
+  "P02": {
+    # Fixed characteristics
+    "from_node": "N02",
+    "to_node": "J01",
+
+    "length": 10.0,                            # m
+    "diameter": 0.75,                          # m, internal diameter
+    "roughness": 0.0001,                       # m = 0.1 mm, typical cast iron pipe with concrete lining
+    
+    # Fittings and bends installed
+    "fittings": [
+      "pipe_bend_45_degrees", 
+      "pipe_bend_90_degrees_long",
+      "pipe_bend_90_degrees_long",
+      "pipe_bend_90_degrees_short",
+    ]
+  },
+  "P03": {
+    # Fixed characteristics
+    "from_node": "J01",
+    "to_node": "N03",
+
+    "length": 10.0,                            # m
+    "diameter": 0.75,                          # m, internal diameter
+    "roughness": 0.0001,                       # m = 0.1 mm, typical cast iron pipe with concrete lining
+    
+    # Fittings and bends installed
+    "fittings": [
+      "pipe_bend_45_degrees", 
+      "pipe_bend_90_degrees_long",
+      "pipe_bend_90_degrees_long",
+      "pipe_bend_90_degrees_short",
+    ]
+  },
 }
 
 ducts = {
@@ -34,13 +103,6 @@ ducts = {
     # Fittings and bends installed
     "fittings": [
       "pipe_bend_45_degrees", 
-      "pipe_bend_90_degrees_long",
-      "pipe_bend_90_degrees_long",
-      "pipe_bend_90_degrees_long",
-      "pipe_bend_90_degrees_short",
-      "pipe_bend_90_degrees_short",
-      "pipe_bend_90_degrees_short",
-      "pipe_bend_90_degrees_short",
     ]
   }
 }
