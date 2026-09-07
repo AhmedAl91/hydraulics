ace078afd108c4b0c2fd0dbb0270a9f876ede421
Ahmed Al-Aaraji <ahmed.al-aaraji@aecom.com>
2026-09-07 16:25:59 +0100

    Main

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
 # Constants
-G = 9.81                                     # m2/s
-KINEMATIC_VISCOSITY = 1.155e-6               # m2/s, @ 15 C
+G = 9.81                # m/s2
+
+KINEMATIC_VISCOSITIES = {
+    10: 1.310e-6,       # m2/s, @ 10 C
+    15: 1.155e-6,       # m2/s, @ 15 C
+}
+KINEMATIC_VISCOSITY = KINEMATIC_VISCOSITIES[10]
